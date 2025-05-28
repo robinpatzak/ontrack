1e3f3e70beb3a6301265d8eb6642d1efe7625e69
Robin Patzak <robin.patzak@proton.me>
2025-05-28 11:11:28 +0200

    Build basic sidebar in dashboard

--- frontend/src/routes/Dashboard.tsx
+++ frontend/src/routes/Dashboard.tsx
@@ -1,26 +1,35 @@
+import AppSidebar from "@/components/AppSidebar";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { useAuth } from "@/contexts/AuthContext";
 import apiClient from "@/lib/api";
 import { useNavigate } from "react-router";
 
 export default function DashboardRoute() {
-  const navigate = useNavigate();
-  const { user } = useAuth();
-
-  const handleLogout = async () => {
-    await apiClient.get("/auth/logout");
-    navigate("/login");
-  };
-
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-xl">
-        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-        <p>
-          Welcome to your dashboard {user?.firstName} {user?.lastName}!
-        </p>
-        <Button onClick={handleLogout}>Logout</Button>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            <h1 className="text-base font-medium">Dashboard</h1>
+          </div>
+        </header>
+
+        <div className="flex flex-1 flex-col gap-4 px-2 py-4 md:gap-6 md:py-6">
+          This is my content
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
