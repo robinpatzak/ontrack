4f7147e8e4156aaa56cc26c979496f1d670e4198
Robin Patzak <robin.patzak@proton.me>
2025-07-13 22:02:22 +0200

    Remove backend port from frontend env since its going through nginx

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,9 @@
 import { API_VERSION } from "@/lib/constants";
-import { BACKEND_BASE_URL, BACKEND_PORT } from "@/lib/env";
+import { BACKEND_BASE_URL } from "@/lib/env";
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `${BACKEND_BASE_URL}:${BACKEND_PORT}/api/${API_VERSION}`,
+  baseURL: `${BACKEND_BASE_URL}/api/${API_VERSION}`,
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -14,5 +14,7 @@ const getEnvVariable = (key: string, defaultValue?: string): string => {
   return value;
 };
 
-export const BACKEND_BASE_URL = getEnvVariable("VITE_BACKEND_BASE_URL", "http://localhost");
-export const BACKEND_PORT = getEnvVariable("VITE_BACKEND_PORT", "8000");
+export const BACKEND_BASE_URL = getEnvVariable(
+  "VITE_BACKEND_BASE_URL",
+  "http://localhost"
+);
